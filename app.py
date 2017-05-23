501101a96a2fdd89fe60d1d25aba69baadbaaa38
Seung Jae (Ryan) Lee <seungjaeryanlee@gmail.com>
2017-05-23 07:00:02 -0400

    Added list of all graphs to the home page

--- app.py
+++ app.py
@@ -1,15 +1,21 @@
 from flask import Flask, render_template
+import json
+
+# Parse data.json
+with open('data.json') as data_file:    
+    data = json.load(data_file)
+graphs = data['graphs']
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return 'Welcome to Graphs DB!'
+    return render_template('home.html', graphs=graphs)
 
-@app.route('/graph')
+@app.route('/graphs')
 def graph_home():
     return 'Here are list of graphs in our database.'
 
-@app.route('/graph/<graphname>')
+@app.route('/graphs/<graphname>')
 def graph(graphname):
     return render_template('graph.html', name=graphname.title(), description='Placeholder for description paragraph')
