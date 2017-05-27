afa2b614bfdf5dcb03690a97832c184c616575c8
Seung Jae (Ryan) Lee <seungjaeryanlee@gmail.com>
2017-05-27 14:43:15 -0400

    Completed transition to list-based properties

--- app.py
+++ app.py
@@ -2,25 +2,8 @@ from flask import Flask, render_template, redirect, url_for, request
 from flask_bootstrap import Bootstrap
 import json
 import random
-from forms import FilterForm, FullForm
+from forms import FullForm, RadioField
 from wtforms.validators import DataRequired
-
-def matches_filter(graph, props):
-    for prop in props:
-        if prop != 'both' and not prop in graph['properties']:
-            return False
-    return True
-
-def encode_props(props):
-    result = ''
-    for prop in props:
-        result += prop + '+'
-
-    return result
-
-def decode_props(raw):
-    props = raw.split('+')[0:-1]
-    return props
 
 def create_app():
     app = Flask(__name__)
@@ -41,29 +24,54 @@ app = create_app()
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
-    form = FullForm(request.form)
-    for prop in properties:
-        new_entry = form.filters.append_entry()
-
-        # Configure new entry with prop
-        new_entry.label = prop['label']
-        new_choices = []
-        for choice in prop['choices']:
-            new_choices.append((choice['name'], choice['label']))
-        new_entry.choices = new_choices
-        new_entry.default = prop['default']
-        if prop['dataRequired'] == 1:
-            new_entry.validators = [DataRequired()]
+    form = FullForm()
 
     if request.method == 'POST' and form.validate():
-        return redirect(url_for('search', encoded_props = encode_props([form.planarity.data, form.directedness.data])))
-    else:
-        return render_template('home.html',form=form)
+        props = []
+        for prop in request.form:
+            if prop != u'submit':
+                props.append(request.form[prop])
+        return redirect(url_for('search', encoded_props = encode_props(props)))
+
+    form = populateForm()
+    return render_template('home.html',form=form)
+    
+def populateForm():
+    form = FullForm()
+
+    # Remove old data
+    while len(form.filters) > 0:
+        form.filters.pop_entry()
+
+    # Add data
+    for prop in properties:
+        new_entry = form.filters.append_entry()
+        new_entry.label = prop['label']
+        new_entry.choices = [(choice['name'], choice['label']) for choice in prop['choices']]
+        new_entry.default = prop['default']
+    return form
+
+def encode_props(props):
+    result = ''
+    for prop in props:
+        result += prop + '+'
+
+    return result
+
+def decode_props(raw):
+    props = raw.split('+')[0:-1]
+    return props
 
 @app.route('/search/<encoded_props>')
 def search(encoded_props):
     # FIXME: Use dictionray instead of list for props
     return render_template('search.html', graphs=graphs, matches_filter=matches_filter, props=decode_props(encoded_props))
+
+def matches_filter(graph, props):
+    for prop in props:
+        if prop != 'both' and not prop in graph['properties']:
+            return False
+    return True
 
 @app.route('/graphs')
 def graph_home():
--- forms.py
+++ forms.py
@@ -1,11 +1,5 @@
 from wtforms import Form, RadioField, FieldList, FormField
 from wtforms.validators import DataRequired
 
-class FilterForm(Form):
-    req = RadioField('Planarity', choices=[('both','Both'), ('planar', 'Planar'), ('nonplanar', 'Nonplanar')], 
-                                  default='both',
-                                  validators=[DataRequired()])
-
 class FullForm(Form):
-    filters = FieldList(RadioField('label', choices=[('both','Both'), ('directed', 'Directed'), ('undirected', 'Undirected')], 
-                                            default='both'))
+    filters = FieldList(RadioField())
